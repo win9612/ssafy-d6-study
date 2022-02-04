4019eb485fe88814e8a99cb3cd6e0f1646046d59
Choi-Seungwon <52640957+choiswonspec@users.noreply.github.com>
2022-02-04 20:08:31 +0900

    Add files via upload

---
+++ seungwon/2022_02_04/Baekjoon1547.java
@@ -0,0 +1,45 @@
+package seungwon;
+
+import java.util.Scanner;
+
+public class Baekjoon1547 {
+
+	public static void main(String[] args) {
+		// TODO 공
+		
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// 인덱스는 0부터 시작하기 때문에 크기가 4인 배열을 생성한다.
+		int[] arr = {0, 1, 0, 0};
+		int temp = 0;
+		// m 입력 받기
+		int m = sc.nextInt();
+		
+		for(int i=0;i<m;i++) {
+			// x 입력 받기
+			int x = sc.nextInt();
+			// y 입력 받기
+			int y = sc.nextInt();
+			
+			// 일시적으로 변수에 값 저장 -> 배열의 값 교환
+			temp = arr[x];
+			arr[x] = arr[y];
+			arr[y] = temp;
+			
+		}
+		// Scanner 닫아줘야함
+		sc.close();
+		
+		// 반복문으로 1의 값을 가지고 있는 인덱스를 출력
+		for(int i=1;i<arr.length;i++) {
+			if(arr[i]==1) {
+				System.out.println(i);
+				break;
+			}
+		}
+
+	}
+
+}
---
+++ seungwon/2022_02_04/Baekjoon4358.java
@@ -0,0 +1,56 @@
+package seungwon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Baekjoon4358 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO 생태학. 나무의 분포도를 측정.
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Map<String, Integer> tree_map = new HashMap<String, Integer>();
+		String str;
+		
+		while( true ) {
+			str = br.readLine();
+			
+			if(str ==null || str.length()==0) {
+				break;
+			}
+			tree_map.put(str, tree_map.getOrDefault(str, 0)+1);
+// getOrDefault(Object key, V DefaultValue) : 찾는 key가 존재하면 찾는 key의 값을 반환하고 없다면 default 값을 반환하는 메서드
+		}
+		
+		// 전체 수 저장할 변수 생성
+		int allcount = 0;
+		// keyset을 만들어서 반복문 ㄱ
+		Set<String> keys = tree_map.keySet();
+		// 반복문으로 allcount 구함
+		for(String key:keys) {
+			allcount += tree_map.get(key);
+		}
+		
+		// 정렬을 위해 Object형 배열 생성
+		Object[] obs = tree_map.keySet().toArray();
+		
+		// 정렬. 이름 알파벳 순.
+		Arrays.sort(obs);
+		
+		for(Object o: obs) {
+			String a = (String) o;
+			int count = tree_map.get(a);
+			double ratio = (double)(count*100.0)/allcount;
+			System.out.printf("%s %.4f\n",a, ratio);
+		}
+		
+		
+
+	}
+
+}
