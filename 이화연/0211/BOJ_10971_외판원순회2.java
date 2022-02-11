d2a4520087f2cd132491c2123c88a21587b09925
yeo_oni <33210124+LeeHwayeon@users.noreply.github.com>
2022-02-11 22:24:08 +0900

    add : [0211] BOJ_10971_외판원순회2 완성

--- 이화연/0211/BOJ_10971_외판원순회2.java
+++ 이화연/0211/BOJ_10971_외판원순회2.java
@@ -13,14 +13,8 @@ public class BOJ_10971_외판원순회2 {
 		N = sc.nextInt(); // 도시 수
 		city = new int[N][N]; // 비용 행렬
 		result = new int[N]; // 순열 저장할 배열
-		numbers = new int[N]; // 도시 1,2,3,4
 		visited = new boolean[N]; // 방문 확인
 		min = Integer.MAX_VALUE;
-
-		int num = 0;
-		for (int i = 0; i < N; i++) {
-			numbers[i] = num++;
-		}
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
@@ -36,28 +30,34 @@ public class BOJ_10971_외판원순회2 {
 	public static void permu(int cnt) {
 		if (cnt == N) {
 			int sum = 0;
-//			System.out.println(Arrays.toString(result));
-			for (int i = 0; i < result.length - 1; i++) {
-				for (int j = i + 1; j <= i + 1; j++) {
-					if (city[result[i]][result[j]] == 0) { // 갈 수 없을 때 빠져나오기..?
-						break;
+			for (int i = 0; i < result.length; i++) {
+				if (i == (result.length - 1)) {
+					if (city[result[result.length - 1]][result[0]] != 0) {
+						// i가 마지막 인덱스일때는 처음으로 다시 가야하기 때문에 city[3][0]을 더해줘야 함
+						sum += city[result[result.length - 1]][result[0]];
+					} else { // 갈 수 없을때
+						return;
 					}
-					// 뽑힌 result 순서에 따라 도시 이동
-					// 예를 들어 result = {0,1,2,3} 일때
-					// city[0][1] -> city[1][2] -> city[2][3] 이렇게..?
-					sum += city[result[i]][result[j]];
+				} else {
+					if (city[result[i]][result[i + 1]] != 0) {
+						// 뽑힌 result 순서에 따라 도시 이동
+						// 예를 들어 result = {0,1,2,3} 일때
+						// city[0][1] -> city[1][2] -> city[2][3] 이렇게
+						sum += city[result[i]][result[i + 1]];
+					} else {
+						return;
+					}
 				}
 			}
+//			System.out.println(sum);
 			min = Math.min(min, sum);
-//			System.out.println(min);
-			return;
 		}
 
 		for (int i = 0; i < N; i++) {
 			if (visited[i])
 				continue;
 
-			result[cnt] = numbers[i];
+			result[cnt] = i;
 			visited[i] = true;
 
 			permu(cnt + 1);
