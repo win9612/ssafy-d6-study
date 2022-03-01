c105fd462c1e68b835ac95c091a724ea0e0a01bf
yeo_oni <33210124+LeeHwayeon@users.noreply.github.com>
2022-03-01 21:46:42 +0900

    update : bfs로 수정

--- 이화연/0301/BOJ_1325_효율적인해킹.java
+++ 이화연/0301/BOJ_1325_효율적인해킹.java
@@ -1,15 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ_1325_효율적인해킹 {
 	static int N, M, cnt[];
-	static List<Integer>[] list;
-	static boolean visited[];
+	static ArrayList<Integer>[] list;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,11 +18,10 @@ public class BOJ_1325_효율적인해킹 {
 		N = Integer.parseInt(st.nextToken()); // 1~N개의 컴퓨터
 		M = Integer.parseInt(st.nextToken()); // 신뢰하는 관계 입력 횟수
 
-		list = new LinkedList[N + 1];
+		list = new ArrayList[N + 1];
 		cnt = new int[N + 1];
-		visited = new boolean[N + 1];
 		for (int n = 1; n <= N; n++) {
-			list[n] = new LinkedList<Integer>(); // 초기화
+			list[n] = new ArrayList<Integer>(); // 초기화
 		}
 
 		for (int m = 0; m < M; m++) {
@@ -37,7 +35,7 @@ public class BOJ_1325_효율적인해킹 {
 		// 컴퓨터 모두 탐색하기
 		for (int n = 1; n <= N; n++) {
 			if (list[n].size() != 0) { // 해당 정점에 인접해있는 게 없으면 탐색할 필요 없음..
-				dfs(n);
+				bfs(n);
 			}
 		}
 
@@ -46,20 +44,32 @@ public class BOJ_1325_효율적인해킹 {
 			max = Math.max(max, cnt[i]); // 가장 큰 값 찾기
 		}
 
+		StringBuilder sb = new StringBuilder();
+
 		for (int i = 1; i <= N; i++) {
 			if (max == cnt[i]) { // 최댓값이랑 같은 것만 출력
-				System.out.print(i + " ");
+				sb.append(i + " ");
 			}
 		}
+		System.out.println(sb.toString());
 
 	}
 
-	public static void dfs(int n) {
+	public static void bfs(int n) {
+		Queue<Integer> queue = new LinkedList<>();
+		boolean visited[] = new boolean[N + 1];
 		visited[n] = true;
-		for (int i = 0; i < list[n].size(); i++) {
-			if (!visited[list[n].get(i)]) {
-				cnt[list[n].get(i)]++;
-				dfs(list[n].get(i));
+		queue.offer(n);
+
+		while (!queue.isEmpty()) {
+			int current = queue.poll();
+
+			for (int i = 0; i < list[current].size(); i++) {
+				if (!visited[list[current].get(i)]) {
+					cnt[list[current].get(i)]++;
+					visited[list[current].get(i)] = true;
+					queue.offer(list[current].get(i));
+				}
 			}
 		}
 	}
