c864d8a82efd40bdf233bae8b18e5bf72a6219bd
daisy6365 <gksekqls9808@inu.ac.kr>
2022-04-26 19:52:12 +0900

    add: 0426

---
+++ 한다빈/0426/문자열내맘대로정렬하기.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class 문자열내맘대로정렬하기 {
+    public static void main(String[] args) {
+        String[] strings = {"sun", "bed", "car"};
+        String[] resultArr = solution(strings, 1);
+        for (String s : resultArr) {
+            System.out.println(s);
+        }
+    }
+    public static String[] solution(String[] strings, int n) {
+        String[] answer = new String[strings.length];
+
+        List<String> tempArr = new ArrayList<>();
+        for (String str : strings) {
+            tempArr.add(str.charAt(n) + str);
+        }
+        Collections.sort(tempArr);
+        for (int i=0; i<tempArr.size(); i++) {
+            answer[i] = tempArr.get(i).substring(1);
+        }
+        return answer;
+    }
+}
